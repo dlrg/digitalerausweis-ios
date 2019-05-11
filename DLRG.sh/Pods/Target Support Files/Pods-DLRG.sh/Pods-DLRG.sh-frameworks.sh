fc66956b57f51a2e21463e82df5f60f94ef9dd5b
Jeremy Boy <github@jboy.eu>
2019-05-11 17:38:13 +0200

    add R.swift

--- DLRG.sh/Pods/Target Support Files/Pods-DLRG.sh/Pods-DLRG.sh-frameworks.sh
+++ DLRG.sh/Pods/Target Support Files/Pods-DLRG.sh/Pods-DLRG.sh-frameworks.sh
@@ -153,10 +153,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/BarcodeScanner/BarcodeScanner.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/BarcodeScanner/BarcodeScanner.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
