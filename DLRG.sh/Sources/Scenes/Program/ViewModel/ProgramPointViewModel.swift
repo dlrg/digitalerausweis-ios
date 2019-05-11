c29f4635d38a46609f46983a8652828ae8358f88
Jeremy Boy <github@jboy.eu>
2019-05-11 23:39:19 +0200

    Display – if location is empty

--- DLRG.sh/Sources/Scenes/Program/ViewModel/ProgramPointViewModel.swift
+++ DLRG.sh/Sources/Scenes/Program/ViewModel/ProgramPointViewModel.swift
@@ -10,15 +10,24 @@ import Foundation
 
 protocol ProgramPointViewModelType {
     var name: String { get }
+    var location: String { get }
     var day: String { get }
 }
 
 struct ProgramPointViewModel: ProgramPointViewModelType {
     let name: String
+    let location: String
     let day: String
 
     init(_ point: ProgramPointType) {
         self.name = point.programmname
+        self.location = point.ort.ifEmpty("–")
         self.day = "Tag \(point.day)"
+    }
+}
+
+private extension String {
+    func ifEmpty(_ default: String) -> String {
+        return isEmpty ? `default` : self
     }
 }
